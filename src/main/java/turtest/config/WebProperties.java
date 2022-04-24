95841577f6a00a318acc192295e2b39139b4e546
zbz <nnzbz@21cn.com>
2022-04-24 19:55:57 +0800

    * 优化代码

--- src/main/java/turtest/config/WebProperties.java
+++ src/main/java/turtest/config/WebProperties.java
@@ -6,5 +6,8 @@ import lombok.Data;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true) // 反序列化时忽略没有的字段
 public class WebProperties {
+    /**
+     * Web服务器监听的端口号
+     */
     private Integer port = 0;
 }
--- src/main/java/turtest/verticle/MainVerticle.java
+++ src/main/java/turtest/verticle/MainVerticle.java
@@ -22,18 +22,12 @@ public class MainVerticle extends AbstractVerticle {
     public void start(Promise<Void> startPromise) throws Exception {
         ConfigRetriever retriever = ConfigRetriever.create(vertx);
         retriever.getConfig(res -> {
-            log.info("config asyncResult: {}", res.result());
+            log.info("config result: {}", res.result());
 
             if (res.failed()) {
                 log.warn("Get config failed", res.cause());
                 startPromise.fail(res.cause());
             }
-
-            vertx.eventBus()
-                    .registerDefaultCodec(Ro.class, new RoCdc())
-                    .registerDefaultCodec(RedisSetCaptchaTo.class, new RedisSetCaptchaToCdc())
-                    .registerDefaultCodec(RedisGetCaptchaTo.class, new RedisGetCaptchaToCdc());
-
 
             JsonObject config = res.result();
             if (config == null || config.isEmpty()) {
@@ -41,8 +35,22 @@ public class MainVerticle extends AbstractVerticle {
                 return;
             }
 
+            // 注册服务调用参数及返回值的解码器
+            vertx.eventBus()
+                    .registerDefaultCodec(Ro.class, new RoCdc())
+                    .registerDefaultCodec(RedisSetCaptchaTo.class, new RedisSetCaptchaToCdc())
+                    .registerDefaultCodec(RedisGetCaptchaTo.class, new RedisGetCaptchaToCdc());
+
+            // 读取main的配置
             MainProperties mainConfig = config.getJsonObject(MainConfig.PREFIX).mapTo(MainProperties.class);
 
+            // 部署verticle
+            Future<String> redisVerticleFuture = vertx.deployVerticle(
+                    RedisVerticle.class,
+                    new DeploymentOptions()
+                            .setInstances(mainConfig.getRedisInstances())
+                            .setWorkerPoolName("vertx-redis-thread")
+                            .setConfig(config.getJsonObject(RedisConfig.PREFIX)));
             Future<String> webVerticleFuture = vertx.deployVerticle(
                     WebVerticle.class,
                     new DeploymentOptions()
@@ -50,14 +58,7 @@ public class MainVerticle extends AbstractVerticle {
                             .setWorkerPoolName("vertx-web-thread")
                             .setConfig(config.getJsonObject(WebConfig.PREFIX)));
 
-            Future<String> redisVerticleFuture = vertx.deployVerticle(
-                    RedisVerticle.class,
-                    new DeploymentOptions()
-                            .setInstances(mainConfig.getRedisInstances())
-                            .setWorkerPoolName("vertx-redis-thread")
-                            .setConfig(config.getJsonObject(RedisConfig.PREFIX)));
-
-            CompositeFuture.all(webVerticleFuture, redisVerticleFuture)
+            CompositeFuture.all(redisVerticleFuture, webVerticleFuture)
                     .onFailure(t -> {
                         startPromise.fail(t);
                         vertx.close();
