40b58783bc4749d566d2b70a5e763363ebf0b53d
zbz <nnzbz@21cn.com>
2022-06-08 22:18:26 +0800

    * 优化代码

--- turtest-svr/src/main/java/myvertx/turtest/verticle/WebVerticle.java
+++ turtest-svr/src/main/java/myvertx/turtest/verticle/WebVerticle.java
@@ -3,24 +3,24 @@ package myvertx.turtest.verticle;
 import static io.vertx.ext.web.validation.builder.Bodies.json;
 import static io.vertx.json.schema.common.dsl.Schemas.objectSchema;
 
-import javax.inject.Inject;
-
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.api.service.RouteToEBServiceHandler;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder;
 import io.vertx.json.schema.SchemaParser;
+import io.vertx.json.schema.SchemaRouter;
+import io.vertx.json.schema.SchemaRouterOptions;
 import myvertx.turtest.api.CaptchaApi;
 import rebue.wheel.vertx.verticle.AbstractWebVerticle;
 
 @SuppressWarnings("deprecation")
 public class WebVerticle extends AbstractWebVerticle {
 
-    @Inject
-    private SchemaParser schemaParser;
-
     @Override
     protected void configRouter(final Router router) {
+        final SchemaParser schemaParser = SchemaParser.createOpenAPI3SchemaParser(
+                SchemaRouter.create(this.vertx, new SchemaRouterOptions()));
+
         // 生成并获取验证码图像
         router.get("/captcha/gen")
                 .handler(RouteToEBServiceHandler.build(
@@ -30,7 +30,7 @@ public class WebVerticle extends AbstractWebVerticle {
         // 校验验证码
         router.post("/captcha/verify")
                 .handler(BodyHandler.create())
-                .handler(ValidationHandlerBuilder.create(this.schemaParser)
+                .handler(ValidationHandlerBuilder.create(schemaParser)
                         .body(json(objectSchema())).build())
                 .handler(RouteToEBServiceHandler.build(
                         this.vertx.eventBus(),
